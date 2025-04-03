5db1551116f5173d5d1615060ba10c60cea11457
IRjSI <rajusuthar0077@gmail.com>
2025-04-03 12:47:58 +0530

    Add sample environment file, update .gitignore, and implement Cloudinary and multer integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/healthCheck.route";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cookieParser());
 
 app.use('/api/v1/healthcheck',router)
 
