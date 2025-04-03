3bd6e4b37dcd8f072b59b1e86ade425ff81fdd1b
IRjSI <rajusuthar0077@gmail.com>
2025-04-03 16:34:52 +0530

    Refactor user registration and model logic, enhance Cloudinary integration, and improve dotenv configuration

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB = async () => {
-    try {
+    try {        
         await mongoose.connect(`${process.env.DB_URL}/vidtubee`).then(() => console.log('Connected to DB'))
     } catch (error) {
         console.log(error);
