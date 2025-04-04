37748e1bd5c841df32123a0f4aab23bacb88b83c
IRjSI <rajusuthar0077@gmail.com>
2025-04-04 20:41:33 +0530

    Add user login functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await UserModel.findById(userId)
+        if (!user) {
+            throw new ApiError(404, 'user not found')
+        }
+    
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        await user.save({ ValidityBeforeSave: false })
+    
+        return { accessToken, refreshToken };
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(500, 'Error generating token')
+    }
+}
+
 const registerUser = asyncHandler(async (req,res) => {
     const { username, fullname, password, email } = req.body; 
     //images(avatar and coverImage) are not coming through 'body' it is coming from 'req.files', handled with multer
@@ -40,4 +60,29 @@ const registerUser = asyncHandler(async (req,res) => {
     return res.json(new ApiResponse(201, newUser, 'User registered'))
 })
 
-export { registerUser };
+const loginUser = asyncHandler(async (req,res) => {
+    const { username, email, password } = req.body
+
+    const user = await UserModel.findOne({
+        $or: [{ username }, { email }]
+    })
+    if (!user) {
+        throw new ApiError(404, 'user not found')
+    }
+
+    const isMatch = await UserModel.isPasswordCorrect(password)
+    if (!isMatch) {
+        throw new ApiError(401, 'Invalid creds')
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production'
+    }
+
+    return res.status(200).cookie('accessToken', accessToken, options).cookie('refreshToken', refreshToken, options).json(new ApiResponse(200, { user }, 'user logged in'))
+})
+
+export { registerUser, loginUser };
