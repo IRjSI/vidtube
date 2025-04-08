a6df47d59f0e88ed9c815a0db1164aed2f27d247
IRjSI <rajusuthar0077@gmail.com>
2025-04-08 20:46:34 +0530

    Implement JWT authentication middleware, add logout functionality, and update user routes

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import UserModel from "../models/user.model";
+
+export const verifyJWT = asyncHandler(async (req,res,next) => {
+    const token = req.cookie.accessToken || req.header('Authorization')?.replace('Beared ', '');
+    if (!token) {
+        throw new ApiError(401, 'Unauth')
+    }
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await UserModel.findById(decodedToken?._id);
+        if (!user) {
+            throw new ApiError(401, 'unautho')
+        }
+
+        req.user = user;
+        next()
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(500, 'something went wrong(auth middleware)')
+    }
+}) 
