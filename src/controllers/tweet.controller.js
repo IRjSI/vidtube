7f3074879a0ac24b01eb6415acab2bb948efab15
IRjSI <rajusuthar0077@gmail.com>
2025-04-09 16:59:02 +0530

    Implement owner validation for CRUD operations and improve error handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,4 @@
-import mongoose from "mongoose"
 import { ApiError } from "../utils/ApiError.js"
-import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { TweetModel } from "../models/tweet.model.js"
@@ -22,14 +20,37 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const tweets = await TweetModel.find({ owner: req.user?._id });
+    if (!tweets) {
+        throw new ApiError(404, 'no tweets found')
+    }
+    
+    return res.status(200).json(new ApiResponse(200, tweets, 'All tweets'))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params;
+    const { newContent } = req.body;
+    const tweet = await TweetModel.findByIdAndUpdate({ _id: tweetId, owner: req.user._id /* only owner can edit */ }, {
+        $set: {
+            content: newContent
+        }
+    }, { new: true })
+    if (!tweet) {
+        throw new ApiError(400, 'problem updating tweet')
+    }
+
+    return res.status(200).json(new ApiResponse(200, tweet, 'tweet updated'))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params;
+
+    await TweetModel.findByIdAndDelete({ _id: tweetId, owner: req.user._id })
+
+    return res.status(200).json(new ApiResponse(200, {}, 'tweet deleted'))
 })
 
 export {
