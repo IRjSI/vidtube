3e946bc3800d44c9be3d2e1f30916eab9ac6ae03
IRjSI <rajusuthar0077@gmail.com>
2025-04-09 19:15:43 +0530

    Add subscription controller with toggle, get subscribers, and get subscribed channels functionalities

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { SubscriptionModel } from "../models/subscription.model.js"
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    // TODO: toggle subscription
+    const {channelId} = req.params
+    const subscriberId = req.user?._id
+
+    if (subscriberId.toString() === channelId.toString()) {
+        throw new ApiError(400, "You cannot subscribe to yourself");
+    }
+    const alreadySubscribed = await SubscriptionModel.findOne({
+        channel: channelId,
+        subscriber: subscriberId
+    })
+    if (alreadySubscribed) {
+        await SubscriptionModel.findByIdAndDelete(alreadySubscribed._id)
+
+        return res.status(200).json(new ApiResponse(200, {}, "Unsubscribed successfully"))
+    } else {
+        const newSubcriber = await SubscriptionModel.create({
+            subscriber: subscriberId,
+            channel: channelId
+        })
+
+        return res.status(200).json(new ApiResponse(201, newSubcriber, "Unsubscribed successfully"))
+    }
+})
+
+// controller to return subscribers list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+    const subscribers = await SubscriptionModel.find({ channel: channelId })
+
+    return res.status(200).json(new ApiResponse(200, subscribers, 'Subscribers list'))
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const subscriberId = req.user._id
+    const subscribedChannels = await SubscriptionModel.find({ subscriber: subscriberId })
+    
+    return res.status(200).json(new ApiResponse(200, subscribedChannels, 'Channels list'))
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
