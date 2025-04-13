116215e59b7a7c731894e5fa3864ffa07ae1c774
IRjSI <rajusuthar0077@gmail.com>
2025-04-13 11:52:35 +0530

    Refactor subscription and video controllers; update subscription logic and improve video retrieval with user data

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { SubscriptionModel } from "../models/subscription.model.js"
+import UserModel from "../models/user.model.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     // TODO: toggle subscription
@@ -25,14 +26,14 @@ const toggleSubscription = asyncHandler(async (req, res) => {
             subscriber: subscriberId,
             channel: channelId
         })
-
+        
         return res.status(200).json(new ApiResponse(201, newSubcriber, "Unsubscribed successfully"))
     }
 })
 
 // controller to return subscribers list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
+    const channelId = req.user._id
     const subscribers = await SubscriptionModel.find({ channel: channelId })
 
     return res.status(200).json(new ApiResponse(200, subscribers, 'Subscribers list'))
@@ -41,7 +42,27 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const subscriberId = req.user._id
-    const subscribedChannels = await SubscriptionModel.find({ subscriber: subscriberId })
+    const subscribedChannels = await SubscriptionModel.aggregate([
+        {
+            "$match": {
+                "subscriber": subscriberId
+            }
+        },
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "channel",
+                "foreignField": "_id",
+                "as": "user"
+            }
+        },
+        {
+            "$project": {
+                "user.username": 1,
+                "user.avatar": 1
+            }
+        }
+    ])
     
     return res.status(200).json(new ApiResponse(200, subscribedChannels, 'Channels list'))
 })
